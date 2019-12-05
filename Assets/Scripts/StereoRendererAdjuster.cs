8df76e4dc63225c6823951224a83bce7faacfe39
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-12-05 21:03:40 +0900

    feat: adjust mirror position when parent is moved

---
+++ Assets/Scripts/StereoRendererAdjuster.cs
@@ -0,0 +1,24 @@
+﻿using HTC.UnityPlugin.StereoRendering;
+
+using UnityEngine;
+
+[RequireComponent(typeof(StereoRenderer))]
+public class StereoRendererAdjuster : MonoBehaviour
+{
+    private Vector3 previousWorldPos;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var worldPos = gameObject.transform.position;
+
+        if (worldPos != previousWorldPos)
+        {
+            previousWorldPos = worldPos;
+
+            var stereoRenderer = gameObject.GetComponent<StereoRenderer>();
+            stereoRenderer.canvasOriginPos = worldPos;
+            stereoRenderer.anchorPos = worldPos;
+        }
+    }
+}
