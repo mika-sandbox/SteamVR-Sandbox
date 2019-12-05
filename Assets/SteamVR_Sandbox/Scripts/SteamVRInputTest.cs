d7e6fe00a80dac531096fd1a6a7322c905b3f0de
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-12-05 23:28:57 +0900

    refactor: apply namespace and change directory

---
+++ Assets/SteamVR_Sandbox/Scripts/SteamVRInputTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using Valve.VR;
+
+namespace SteamVR_Sandbox.Scripts
+{
+    // ReSharper disable once InconsistentNaming
+    public class SteamVRInputTest : MonoBehaviour
+    {
+        public SteamVR_Action_Boolean GrabAction;
+        public SteamVR_Input_Sources HandType;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (GrabAction.GetState(HandType)) Debug.Log(GrabAction.ToString());
+        }
+    }
+}
---
+++ Assets/SteamVR_Sandbox/Scripts/StereoRendererAdjuster.cs
@@ -0,0 +1,27 @@
+﻿using HTC.UnityPlugin.StereoRendering;
+
+using UnityEngine;
+
+namespace SteamVR_Sandbox.Scripts
+{
+    [RequireComponent(typeof(StereoRenderer))]
+    public class StereoRendererAdjuster : MonoBehaviour
+    {
+        private Vector3 previousWorldPos;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            var worldPos = gameObject.transform.position;
+
+            if (worldPos != previousWorldPos)
+            {
+                previousWorldPos = worldPos;
+
+                var stereoRenderer = gameObject.GetComponent<StereoRenderer>();
+                stereoRenderer.canvasOriginPos = worldPos;
+                stereoRenderer.anchorPos = worldPos;
+            }
+        }
+    }
+}
