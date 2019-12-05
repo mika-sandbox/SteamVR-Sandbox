737a90a1d6c08296693269ba1282267f63f1c7ae
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-12-05 23:29:22 +0900

    feat: controller adjuster

---
+++ Assets/SteamVR_Sandbox/Models/ControllerDefinition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace SteamVR_Sandbox.Models
+{
+    public class ControllerDefinition
+    {
+        public static ControllerDefinition OculusTouch { get; }
+
+        public Vector3 LeftHandPosition { get; private set; }
+        public Quaternion LeftHandRotation { get; private set; }
+
+        public Vector3 RightHandPosition { get; private set; }
+        public Quaternion RightHandRotation { get; private set; }
+
+        static ControllerDefinition()
+        {
+            OculusTouch = new ControllerDefinition
+            {
+                LeftHandPosition = new Vector3(0, .025f, .11f),
+                LeftHandRotation = Quaternion.Euler(5, 90, 105),
+                RightHandPosition = new Vector3(0, -.025f, .11f),
+                RightHandRotation = Quaternion.Euler(5, -90, -105)
+            };
+        }
+    }
+}
---
+++ Assets/SteamVR_Sandbox/Scripts/SteamVRControllerCalibrator.cs
@@ -0,0 +1,62 @@
+﻿using SteamVR_Sandbox.Models;
+
+using UnityEngine;
+
+using Valve.VR;
+
+// ReSharper disable once InconsistentNaming
+namespace SteamVR_Sandbox.Scripts
+{
+    public class SteamVRControllerCalibrator : MonoBehaviour
+    {
+        private bool _isFixed;
+
+        [SerializeField]
+        private GameObject Model;
+
+        [SerializeField]
+        private GameObject Tracker;
+
+        public SteamVRControllerCalibrator()
+        {
+            _isFixed = false;
+        }
+
+        // Start is called before the first frame update
+        private void Update()
+        {
+            if (_isFixed)
+                return;
+
+            var model = Model.GetComponent<SteamVR_RenderModel>();
+            if (model == null)
+                return;
+
+            var controller = model.renderModelName;
+            if (string.IsNullOrWhiteSpace(controller))
+                return;
+
+            var position = Vector3.zero;
+            var rotation = Quaternion.identity;
+
+            switch (controller)
+            {
+                case "oculus_rifts_controller_right":
+                    position = ControllerDefinition.OculusTouch.RightHandPosition;
+                    rotation = ControllerDefinition.OculusTouch.RightHandRotation;
+
+                    break;
+
+                case "oculus_rifts_controller_left":
+                    position = ControllerDefinition.OculusTouch.LeftHandPosition;
+                    rotation = ControllerDefinition.OculusTouch.LeftHandRotation;
+                    break;
+            }
+
+            Tracker.transform.localPosition = position;
+            Tracker.transform.localRotation = rotation;
+
+            _isFixed = true;
+        }
+    }
+}
