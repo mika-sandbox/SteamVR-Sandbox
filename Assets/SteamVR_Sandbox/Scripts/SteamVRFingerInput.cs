51d38f7d4884708df6e27b88b6c849c70f36d417
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-12-09 18:44:20 +0900

    fix: missing meta file

---
+++ Assets/SteamVR_Sandbox/Scripts/SteamVRFingerInput.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace SteamVR_Sandbox.Scripts
+{
+    // ReSharper disable once InconsistentNaming
+    public class SteamVRFingerInput : MonoBehaviour
+    {
+        public enum FingerCategory
+        {
+            RightIndex,
+
+            RightLittle,
+
+            RightMiddle,
+
+            RightRing,
+
+            RightThumb,
+
+            LeftIndex,
+
+            LeftLittle,
+
+            LeftMiddle,
+
+            LeftRing,
+
+            LeftThumb
+        }
+
+		[SerializeField]
+		private Animator Avatar;
+
+		[SerializeField]
+        private FingerCategory Finger;
+
+		private HumanPose _humanPose;
+
+		// Start is called before the first frame update
+		private void Start() { }
+
+        // Update is called once per frame
+        private void Update()
+        { 
+            if (this.Avatar == null) {
+				return;
+			}
+
+			var handler = new HumanPoseHandler(this.Avatar.avatar, this.Avatar.transform);
+			handler.GetHumanPose(ref this._humanPose);
+
+			handler.SetHumanPose(ref this._humanPose);
+		}
+    }
+}
