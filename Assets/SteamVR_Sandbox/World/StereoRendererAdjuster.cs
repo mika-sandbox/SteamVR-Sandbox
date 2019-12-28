6eeefd35c3b569c91fdd3eb68913066b5c2321b4
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-12-28 22:00:56 +0900

    refactor: Scripts to World

--- Assets/SteamVR_Sandbox/World/StereoRendererAdjuster.cs
+++ Assets/SteamVR_Sandbox/World/StereoRendererAdjuster.cs
@@ -2,21 +2,22 @@
 
 using UnityEngine;
 
-namespace SteamVR_Sandbox.Scripts
+namespace SteamVR_Sandbox.World
 {
+    [AddComponentMenu("Scripts/Mochizuki.VR/World/Stereo Renderer Adjuster")]
     [RequireComponent(typeof(StereoRenderer))]
     public class StereoRendererAdjuster : MonoBehaviour
     {
-        private Vector3 previousWorldPos;
+        private Vector3 _previousWorldPos;
 
         // Update is called once per frame
         private void Update()
         {
             var worldPos = gameObject.transform.position;
 
-            if (worldPos != previousWorldPos)
+            if (worldPos != _previousWorldPos)
             {
-                previousWorldPos = worldPos;
+                _previousWorldPos = worldPos;
 
                 var stereoRenderer = gameObject.GetComponent<StereoRenderer>();
                 stereoRenderer.canvasOriginPos = worldPos;
