63ebcf6a620857df9534a1727c5950d8a65469a2
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-07 01:53:30 +0900

    feat: use Unity Input Module for SteamVR laser pointer input

--- Assets/SteamVR_Sandbox/Avatar/AvatarLaserPointer.cs
+++ Assets/SteamVR_Sandbox/Avatar/AvatarLaserPointer.cs
@@ -1,33 +1,21 @@
-﻿using System.Linq;
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-using Valve.VR;
+using static SteamVR_Sandbox.SteamVR.SteamVRInputModule;
 
 namespace SteamVR_Sandbox.Avatar
 {
-    public class AvatarLaserPointer : MonoBehaviour
+    public class AvatarLaserPointer : LaserPointerRaycastReceiver
     {
         private GameObject _laser;
         private GameObject _pointer;
-
-        private Transform _previousContact;
-
-        private Scene _scene;
-
-        // ReSharper disable once FieldCanBeMadeReadOnly.Local
-        [SerializeField]
-        private SteamVR_Action_Boolean InteractUI = SteamVR_Input.GetBooleanAction("InteractUI");
 
         [SerializeField]
         private Material LaserMaterial;
 
         // ReSharper disable once FieldCanBeMadeReadOnly.Local
         [SerializeField]
-        private float LaserThickness = 0.01f;
+        private float LaserThickness = 0.001f;
 
         [SerializeField]
         private Material PointerMaterial;
@@ -36,19 +24,31 @@ namespace SteamVR_Sandbox.Avatar
         [SerializeField]
         private float PointerRadius = 0.05f;
 
-        [SerializeField]
-        private SteamVR_Behaviour_Pose Pose;
+        public override void OnUpdate(RaycastResult raycast)
+        {
+            if (raycast.gameObject)
+            {
+                _laser.transform.localScale = new Vector3(LaserThickness * 4f, LaserThickness * 4f, raycast.distance);
+                _laser.transform.localPosition = new Vector3(0f, 0f, raycast.distance / 2f);
+
+                _pointer.transform.position = raycast.worldPosition;
+                _pointer.SetActive(true);
+            }
+            else
+            {
+                _laser.transform.localScale = new Vector3(LaserThickness, LaserThickness, 0f);
+                _laser.transform.localPosition = new Vector3(0f, 0f, 0f);
+
+                _pointer.SetActive(false);
+            }
+        }
 
         private void Start()
         {
-            if (InteractUI == null)
-                Debug.LogError("No UI interaction action has been set on this component", this);
             if (LaserMaterial == null)
                 Debug.LogWarning("No Laser Material found on this component", this);
             if (PointerMaterial == null)
                 Debug.LogWarning("No Pointer Material found on this component", this);
-            if (Pose == null)
-                Debug.LogError("No SteamVR_Behaviour_Pose component found on this component", this);
 
             _laser = GameObject.CreatePrimitive(PrimitiveType.Cube);
             _laser.transform.parent = transform;
@@ -63,134 +63,6 @@ namespace SteamVR_Sandbox.Avatar
             _pointer.transform.localPosition = new Vector3(0f, 0f, 0f);
             _pointer.GetComponent<MeshRenderer>().material = PointerMaterial;
             _pointer.SetActive(false);
-
-            _scene = SceneManager.GetActiveScene();
-        }
-
-        private void Update()
-        {
-            // is hit to physics objects?
-            var ray = new Ray(transform.position, transform.forward);
-
-            // Physics.Raycast(ray, out var hitToObject, 100f);
-
-            // is hit to uGUI objects?
-            var hitToGui = RaycastToGuiObjects(ray, 100f);
-
-            var nearestHitObject = GetNearestObjectFromPosition( /* new HitTransform(hitToObject), */ hitToGui);
-            if (nearestHitObject == null)
-            {
-                // no hit
-                _laser.transform.localScale = new Vector3(LaserThickness, LaserThickness, 0f);
-                _laser.transform.localPosition = new Vector3(0f, 0f, 0f);
-                _pointer.SetActive(false);
-            }
-
-            // press downed
-            var data = new PointerEventData(EventSystem.current);
-
-            if (nearestHitObject != null)
-            {
-                data.position = nearestHitObject.Position;
-                data.button = PointerEventData.InputButton.Left;
-
-                _laser.transform.localScale = new Vector3(LaserThickness * 4f, LaserThickness * 4f, nearestHitObject.Distance);
-                _laser.transform.localPosition = new Vector3(0f, 0f, nearestHitObject.Distance / 2f);
-
-                _pointer.transform.position = nearestHitObject.Position;
-                _pointer.SetActive(true);
-
-                if (InteractUI?.GetState(Pose.inputSource) == true)
-                {
-                    if (_previousContact == null)
-                    {
-                        _previousContact = nearestHitObject.Transform;
-                        ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.pointerDownHandler);
-                        ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.pointerClickHandler);
-                        ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.beginDragHandler);
-                    }
-                    else if (nearestHitObject.Transform != _previousContact)
-                    {
-                        ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.endDragHandler);
-                        ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.pointerUpHandler);
-                        _previousContact = nearestHitObject.Transform;
-                    }
-                    else
-                    {
-                        ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.dragHandler);
-                    }
-
-                    return;
-                }
-            }
-
-            if (_previousContact == null)
-                return;
-
-            ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.endDragHandler);
-            ExecuteEvents.Execute(_previousContact.gameObject, data, ExecuteEvents.pointerUpHandler);
-            _previousContact = null;
-        }
-
-        private HitTransform RaycastToGuiObjects(Ray ray, float maxDistance)
-        {
-            var canvases = _scene.GetRootGameObjects().SelectMany(w => w.GetComponentsInChildren<Canvas>());
-
-            // see: http://edom18.hateblo.jp/entry/2019/12/20/121928
-            foreach (var graphic in canvases.SelectMany(GraphicRegistry.GetGraphicsForCanvas).ToList())
-            {
-                if (graphic.depth == -1 || !graphic.raycastTarget)
-                    continue;
-
-                // ReSharper disable once LocalVariableHidesMember
-                var transform = graphic.transform;
-                var direction = Vector3.Dot(transform.forward, ray.direction);
-
-                if (direction <= 0)
-                    continue;
-
-                var distance = Vector3.Dot(transform.forward, transform.position - ray.origin) / direction;
-                if (distance >= maxDistance)
-                    continue;
-
-                var position = ray.GetPoint(distance);
-                var pointerPos = Camera.main.WorldToScreenPoint(position);
-
-                if (!RectTransformUtility.RectangleContainsScreenPoint(graphic.rectTransform, pointerPos, Camera.main))
-                    continue;
-                if (!graphic.Raycast(pointerPos, Camera.main))
-                    continue;
-
-                return new HitTransform(distance, position, graphic.transform);
-            }
-
-            return null;
-        }
-
-        private static HitTransform GetNearestObjectFromPosition(params HitTransform[] hits)
-        {
-            return hits.Where(w => w?.Transform != null).OrderBy(w => w.Distance).FirstOrDefault();
-        }
-
-        private class HitTransform
-        {
-            public float Distance { get; }
-            public Vector3 Position { get; }
-            public Transform Transform { get; }
-
-            public HitTransform(RaycastHit hit)
-            {
-                Distance = hit.distance;
-                Position = hit.point;
-                Transform = hit.transform;
-            }
-
-            public HitTransform(float distance, Vector3 position, Transform transform)
-            {
-                Distance = distance;
-                Position = position;
-                Transform = transform;
-            }
         }
     }
 }
---
+++ Assets/SteamVR_Sandbox/SteamVR/SteamVRInputModule.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using Valve.VR;
+
+namespace SteamVR_Sandbox.SteamVR
+{
+    [AddComponentMenu("Scripts/Mochizuki.VR/SteamVR/SteamVR Input Module")]
+    public class SteamVRInputModule : BaseInputModule
+    {
+        private List<RaycastResult> _raycastResultsCache;
+        private Camera _uiCamera;
+
+        [SerializeField]
+        private InputSource InputSourceLeft;
+
+        [SerializeField]
+        private InputSource InputSourceRight;
+
+        // ReSharper disable once FieldCanBeMadeReadOnly.Local
+        [SerializeField]
+        private SteamVR_Action_Boolean InteractUI = SteamVR_Input.GetBooleanAction("InteractUI");
+
+        private List<InputSource> Poses => new List<InputSource> { InputSourceLeft, InputSourceRight };
+
+        protected override void Start()
+        {
+            base.Start();
+
+            if (InteractUI == null)
+                Debug.LogError("No UI interaction action has been set on this component", this);
+            InputSourceLeft.Initialize(eventSystem);
+            InputSourceRight.Initialize(eventSystem);
+
+            // Camera for UI detection, not rendering objects
+            _uiCamera = new GameObject("UI Camera").AddComponent<Camera>();
+            _uiCamera.clearFlags = CameraClearFlags.Nothing;
+            _uiCamera.cullingMask = 0;
+            _uiCamera.enabled = false;
+            _uiCamera.fieldOfView = 1;
+            _uiCamera.nearClipPlane = 0.01f;
+
+            foreach (var canvas in Resources.FindObjectsOfTypeAll<Canvas>())
+                canvas.worldCamera = _uiCamera;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _raycastResultsCache = new List<RaycastResult>();
+        }
+
+        public override void Process()
+        {
+            if (!InputSourceLeft.Validate() || !InputSourceRight.Validate())
+                return;
+
+            Poses.ForEach(ProcessEvents);
+        }
+
+        private void ProcessEvents(InputSource source)
+        {
+            UpdateCameraPositionTo(source.Pose.transform);
+
+            source.EventData.Reset();
+            source.EventData.position = new Vector2(_uiCamera.pixelWidth * 0.5f, _uiCamera.pixelHeight * 0.5f);
+
+            eventSystem.RaycastAll(source.EventData, _raycastResultsCache);
+            source.EventData.pointerCurrentRaycast = FindFirstRaycast(_raycastResultsCache);
+            source.Receiver?.OnUpdate(source.EventData.pointerCurrentRaycast);
+            _raycastResultsCache.Clear();
+
+            HandlePointerExitAndEnter(source.EventData, source.EventData.pointerCurrentRaycast.gameObject);
+
+            if (InteractUI.GetState(source.Pose.inputSource))
+            {
+                if (source.PreviousContactObject == null)
+                {
+                    HandlePress(source);
+                }
+                else if (source.PreviousContactObject != source.EventData.pointerCurrentRaycast.gameObject)
+                {
+                    HandleRelease(source);
+                    HandlePress(source);
+                }
+                else
+                {
+                    // drag
+                    ExecuteEvents.Execute(source.PreviousContactObject, source.EventData, ExecuteEvents.dragHandler);
+                }
+
+                return;
+            }
+
+            if (source.PreviousContactObject) HandleRelease(source);
+        }
+
+        // ReSharper disable once ParameterHidesMember
+        private void UpdateCameraPositionTo(Transform transform)
+        {
+            _uiCamera.transform.position = transform.position;
+            _uiCamera.transform.rotation = transform.rotation;
+        }
+
+        private void HandlePress(InputSource source)
+        {
+            // press
+            source.PreviousContactObject = source.EventData.pointerCurrentRaycast.gameObject;
+            source.EventData.pointerPressRaycast = source.EventData.pointerCurrentRaycast;
+
+            var pressed = ExecuteEvents.ExecuteHierarchy(source.PreviousContactObject, source.EventData, ExecuteEvents.pointerDownHandler);
+            if (pressed == null)
+            {
+                pressed = ExecuteEvents.GetEventHandler<IPointerClickHandler>(source.PreviousContactObject);
+                ExecuteEvents.Execute(source.PreviousContactObject, source.EventData, ExecuteEvents.pointerClickHandler);
+                ExecuteEvents.Execute(source.PreviousContactObject, source.EventData, ExecuteEvents.beginDragHandler);
+            }
+            else
+            {
+                ExecuteEvents.Execute(pressed, source.EventData, ExecuteEvents.pointerClickHandler);
+                ExecuteEvents.Execute(pressed, source.EventData, ExecuteEvents.beginDragHandler);
+                ExecuteEvents.Execute(source.PreviousContactObject, source.EventData, ExecuteEvents.pointerClickHandler);
+                ExecuteEvents.Execute(source.PreviousContactObject, source.EventData, ExecuteEvents.beginDragHandler);
+            }
+
+            if (pressed != null)
+            {
+                source.EventData.pressPosition = pressed.transform.position;
+                eventSystem.SetSelectedGameObject(pressed);
+            }
+
+            source.EventData.pointerPress = pressed;
+            source.EventData.pointerDrag = pressed;
+            source.EventData.rawPointerPress = source.PreviousContactObject;
+        }
+
+        private void HandleRelease(InputSource source)
+        {
+            // release
+            ExecuteEvents.Execute(source.EventData.pointerPress, source.EventData, ExecuteEvents.pointerUpHandler);
+            ExecuteEvents.Execute(source.EventData.pointerDrag, source.EventData, ExecuteEvents.endDragHandler);
+
+            eventSystem.SetSelectedGameObject(null);
+
+            source.EventData.pressPosition = Vector2.zero;
+            source.EventData.pointerPress = null;
+            source.EventData.pointerDrag = null;
+            source.EventData.rawPointerPress = null;
+            source.PreviousContactObject = null;
+        }
+
+        [Serializable]
+        private class InputSource
+        {
+            public PointerEventData EventData { get; private set; }
+            public GameObject PreviousContactObject { get; set; }
+
+            public void Initialize(EventSystem eventSystem)
+            {
+                if (Pose == null)
+                    Debug.LogError("No SteamVR_Behaviour_Pose component found on this component");
+
+                EventData = new PointerEventData(eventSystem);
+                PreviousContactObject = null;
+            }
+
+            public bool Validate()
+            {
+                return Pose != null;
+            }
+
+            #region Pose
+
+            [SerializeField]
+            private SteamVR_Behaviour_Pose _pose;
+
+            public SteamVR_Behaviour_Pose Pose
+            {
+                get => _pose;
+                set => _pose = value;
+            }
+
+            #endregion
+
+            #region Receiver
+
+            [SerializeField]
+            private LaserPointerRaycastReceiver _receiver;
+
+            public LaserPointerRaycastReceiver Receiver
+            {
+                get => _receiver;
+                set => _receiver = value;
+            }
+
+            #endregion
+        }
+
+        public abstract class LaserPointerRaycastReceiver : MonoBehaviour
+        {
+            public abstract void OnUpdate(RaycastResult raycast);
+        }
+    }
+}
