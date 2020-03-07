38a725e879f26c96d21a9b380b42b52c5f7c984f
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-07 17:43:50 +0900

    feat: add NumericUpDown control

---
+++ Assets/SteamVR_Sandbox/UI/NumericUpDown.cs
@@ -0,0 +1,64 @@
+﻿using TMPro;
+
+using UnityEngine;
+
+namespace SteamVR_Sandbox.UI
+{
+    public class NumericUpDown : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI Label;
+
+        [SerializeField]
+        private int MaxValue;
+
+        [SerializeField]
+        private int MinValue;
+
+        // ReSharper disable once FieldCanBeMadeReadOnly.Local
+        [SerializeField]
+        private int Step = 1;
+
+        public void OnClickUpButton()
+        {
+            var newValue = Value + Step;
+            if (newValue > MaxValue)
+                return;
+
+            Value = newValue;
+        }
+
+        public void OnClickDownButton()
+        {
+            var newValue = Value - Step;
+            if (newValue < MinValue)
+                return;
+
+            Value = newValue;
+        }
+
+        private void SetValueToLabel(int value)
+        {
+            Label.text = $"{value}";
+        }
+
+        #region Value
+
+        [SerializeField]
+        private int _value;
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                    return;
+                SetValueToLabel(value);
+                _value = value;
+            }
+        }
+
+        #endregion
+    }
+}
